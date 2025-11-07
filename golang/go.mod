1af81a2c3e7b6993dcaa41495029fe179388da13
Berend-Jan Lange <berendjanlange@Mac.home>
2025-11-07 20:55:14 +0100

    first commit

---
+++ golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/berendjan/golang-bazel-starter
+
+go 1.25.4
---
+++ golang/grpc-server/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	configpb "github.com/berendjanlange/golang-bazel-starter/proto/configuration_service/v1"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+func main() {
+	flag.Parse()
+
+	// Create listener
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	// Create gRPC server
+	grpcServer := grpc.NewServer()
+
+	// Register the Configuration service
+	configServer := NewConfigurationServer()
+	configpb.RegisterConfigurationServer(grpcServer, configServer)
+
+	// Register reflection service for grpcurl/grpc_cli
+	reflection.Register(grpcServer)
+
+	// Setup graceful shutdown
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+		<-sigCh
+		log.Println("Shutting down gRPC server...")
+		grpcServer.GracefulStop()
+	}()
+
+	log.Printf("Starting gRPC server on port %d", *port)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
